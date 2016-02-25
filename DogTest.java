896f2b7b290b65b7464b0fce4f2ae6917a29234b
JackNel <jcneligan@gmail.com>
2016-02-25 15:57:58 -0500

    Create DogTest, example of altering an instance methods behavior based on the input

---
+++ DogTest.java
@@ -0,0 +1,35 @@
+// Example of an instance method thats behavior changes depending on the value of
+// the instance variable "size"
+
+class Dog {
+    int size;
+    String name;
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Woof! Woof!");
+        }
+        else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        }
+        else {
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
+
+public class DogTest {
+    public static void main(String[] args) {
+        Dog one = new Dog();
+        one.size = 70;
+
+        Dog two = new Dog();
+        two.size = 8;
+        Dog three = new Dog();
+        three.size = 35;
+
+        one.bark();
+        two.bark();
+        three.bark();
+    }
+}
